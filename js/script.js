39c36688110fb8f824533eb48675ad830c4bffc2
SamerSenbol <samer.senbol@gmail.com>
2022-09-19 10:02:28 +0200

    fix #1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-alert('samer')
+// Variables
+const tweetList = document.getElementById('tweet-list');
+
+// Event Listeners
+eventListeners();
+
+function eventListeners() {
+    // Form Submission
+    document.querySelector('#form').addEventListener('submit', newTweet);
+}
+
+// Functions
+function newTweet(e) {
+    e.preventDefault();
+
+    // Read the textarea value
+    const tweet = document.getElementById('tweet').value;
+
+    // Create the remove button
+    const removeBtn = document.createElement('a');
+    removeBtn.classList = 'remove-tweet';
+    removeBtn.textContent = 'X';
+
+    // Create an <li> element
+    const li = document.createElement('li');
+    li.textContent = tweet;
+
+    // Add the remove button to each tweet
+    li.appendChild(removeBtn);
+
+    // Add to the list
+    tweetList.appendChild(li)
+    
+    this.reset();
+}
